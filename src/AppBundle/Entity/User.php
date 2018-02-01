90e3e9750147fcf81723878d523b581aefa60ac6
Ashish Bhatt <ashishbhattcool@gmail.com>
2018-02-01 16:42:03 +0530

    FOS user bundle configuration,user entity creation

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+// src/AppBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="`user`")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=50)
+     */
+    protected $name;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
